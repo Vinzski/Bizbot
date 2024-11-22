ace11b02bde9e8fe7f5a4ea985b1e68bac3a6ba1
Vinzski <vinzmuloc@gmail.com>
2024-11-22 17:51:56 +0800

    Resolve conflicts and add changes

--- src/node/signup/server.js
+++ src/node/signup/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../../public')));  // Adjust as necessary
 
-connectDB();  // Connect to MongoDB
+connectDB();
 
 app.use(cors({
     origin: '*',
