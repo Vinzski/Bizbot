820818e962b710bc5fda1749f5ae2d3111e79ebb
Vinzski <153497509+Vinzski@users.noreply.github.com>
2024-11-22 17:30:16 +0800

    Update server.js

--- src/node/signup/server.js
+++ src/node/signup/server.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 const path = require('path');
 const connectDB = require('../config/db');
-const userModel = require('../../models/userModel');
+const userModel = require('../models/userModel');
 
 
 app.use(express.json());
