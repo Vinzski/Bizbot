651eb5526bdaefac1fe12e1b57f6fc8cf0bee3b7
Vinzski <153497509+Vinzski@users.noreply.github.com>
2024-11-25 00:23:45 +0800

    Update db.js

--- src/node/config/db.js
+++ src/node/config/db.js
@@ -2,12 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const mongoURI = process.env.MONGO_URI || 'mongodb+srv://admin:admin12345@cluster-url/database?retryWrites=true&w=majority'; 
-        await mongoose.connect(mongoURI);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('Failed to connect to MongoDB', error);
-        process.exit(1);
     }
 };
 
