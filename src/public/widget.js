de04e523c1b72ff4430c2c16d69d2de4b1d4598e
Vinzski <153497509+Vinzski@users.noreply.github.com>
2024-11-27 01:51:22 +0800

    Update widget.js

--- src/public/widget.js
+++ src/public/widget.js
@@ -87,7 +87,7 @@
         chatMessages.appendChild(userMessageElement);
 
         // Send the message to the API
-        fetch('https://empty-ideas-lick.loca.lt/api/chat', {
+        fetch('/api/chat', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
