743eb9c46f118b2fc3c675d3c855417e1f6d8c0b
Vinzski <153497509+Vinzski@users.noreply.github.com>
2024-11-27 00:05:23 +0800

    Update chatRoutes.js

--- src/node/api/chatRoutes.js
+++ src/node/api/chatRoutes.js
@@ -32,7 +32,7 @@ router.post('/', authenticate, async (req, res) => {
     } else {
         // If no FAQ matches well, send the query to Rasa
         try {
-            const rasaResponse = await axios.post('https://social-wombats-rest.loca.lt/webhooks/rest/webhook', {
+            const rasaResponse = await axios.post('https://eight-women-fly.loca.lt/webhooks/rest/webhook', {
                 message: question,
                 sender: 'chatbot-widget'
             });
@@ -77,7 +77,7 @@ router.post('/chat', authenticate, async (req, res) => {
     } else {
         // If no FAQ matches well, send the query to Rasa
         try {
-            const rasaResponse = await axios.post('https://social-wombats-rest.loca.lt/webhooks/rest/webhook', {
+            const rasaResponse = await axios.post('https://eight-women-fly.loca.lt/webhooks/rest/webhook', {
                 message: question,
                 sender: 'chatbot-widget'
             });
