2e5c99e76cd658f96f38cdd285a88a75ca6e29e9
Vinzski <153497509+Vinzski@users.noreply.github.com>
2024-11-27 00:55:35 +0800

    Update widget.js

--- src/public/widget.js
+++ src/public/widget.js
@@ -87,7 +87,7 @@
         chatMessages.appendChild(userMessageElement);
 
         // Send the message to the API
-        fetch('https://bizbot-khpq.onrender.com/api/chat', {
+        fetch('https://eight-women-fly.loca.lt/api/chat', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
