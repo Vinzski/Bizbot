f6d536b2e6182662c04eeb34b45e89aaed11a3ef
Vinzski <153497509+Vinzski@users.noreply.github.com>
2024-11-27 20:16:27 +0800

    Update chatRoutes.js

--- src/node/api/chatRoutes.js
+++ src/node/api/chatRoutes.js
@@ -32,7 +32,7 @@ router.post('/', authenticate, async (req, res) => {
     } else {
         // If no FAQ matches well, send the query to Rasa
         try {
-            const rasaResponse = await axios.post('https://better-chicken-doubt.loca.lt/webhooks/rest/webhook', {
+            const rasaResponse = await axios.post('https://huge-parrots-push.loca.lt/webhooks/rest/webhook', {
                 message: question,
                 sender: 'chatbot-widget'
             });
@@ -77,7 +77,7 @@ router.post('/chat', authenticate, async (req, res) => {
     } else {
         // If no FAQ matches well, send the query to Rasa
         try {
-            const rasaResponse = await axios.post('https://better-chicken-doubt.loca.lt/webhooks/rest/webhook', {
+            const rasaResponse = await axios.post('https://huge-parrots-push.loca.lt/webhooks/rest/webhook', {
                 message: question,
                 sender: 'chatbot-widget'
             });
