c2c7b3808744f388b22272b25dc706beb60540e3
Vinzski <153497509+Vinzski@users.noreply.github.com>
2024-11-30 17:31:56 +0800

    Update chatRoutes.js

--- src/node/api/chatRoutes.js
+++ src/node/api/chatRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const authenticate = require('../signup/middleware/authMiddleware');  // Add path to your auth middleware
 
 const authenticateByDomain = (req, res, next) => {
-    const allowedDomains = ['http://localhost:8080/', 'http://localhost:8080/index.html'];
+    const allowedDomains = ['http://localhost:3000/index.html'];
     const refererHeader = req.headers.referer;
 
     if (refererHeader && allowedDomains.some(domain => refererHeader.startsWith(domain))) {
