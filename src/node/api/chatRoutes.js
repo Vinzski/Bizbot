91ddc6a6a4c912e245bbde2b4f90c0691601f72b
Vinzski <153497509+Vinzski@users.noreply.github.com>
2024-11-30 15:23:06 +0800

    Update chatRoutes.js

--- src/node/api/chatRoutes.js
+++ src/node/api/chatRoutes.js
@@ -25,7 +25,7 @@ router.post('/send_message', (req, res) => {
     res.json({reply: "Response based on " + userMessage});
 });
 
-router.post('/chat', authenticateByDomain, async (req, res) => {
+router.post('/chat', authenticateByDomain, authenticate, async (req, res) => {
     const { question, chatbotId } = req.body;
     const userId = req.user.id;
 
