c676f1d530778269ad9029f2058f1ac59ae5d783
Vinzski <153497509+Vinzski@users.noreply.github.com>
2024-12-01 17:54:31 +0800

    Update chatRoutes.js

--- src/node/api/chatRoutes.js
+++ src/node/api/chatRoutes.js
@@ -32,7 +32,7 @@ router.post('/', authenticate, async (req, res) => {
     } else {
         // If no FAQ matches well, send the query to Rasa
         try {
-            const rasaResponse = await axios.post('https://poor-times-sleep.loca.lt/webhooks/rest/webhook', {
+            const rasaResponse = await axios.post('https://odd-bags-raise.loca.lt/webhooks/rest/webhook', {
                 message: question,
                 sender: 'chatbot-widget'
             });
@@ -77,7 +77,7 @@ router.post('/chat', authenticate, async (req, res) => {
     } else {
         // If no FAQ matches well, send the query to Rasa
         try {
-            const rasaResponse = await axios.post('https://poor-times-sleep.loca.lt/webhooks/rest/webhook', {
+            const rasaResponse = await axios.post('https://odd-bags-raise.loca.lt/webhooks/rest/webhook', {
                 message: question,
                 sender: 'chatbot-widget'
             });
@@ -89,3 +89,5 @@ router.post('/chat', authenticate, async (req, res) => {
         }
     }
 });
+
+module.exports = router;
