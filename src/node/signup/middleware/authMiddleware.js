d66c926d589199f23f904e5268f999d15818a1bb
Vinzski <153497509+Vinzski@users.noreply.github.com>
2024-12-02 11:28:46 +0800

    Update authMiddleware.js

--- src/node/signup/middleware/authMiddleware.js
+++ src/node/signup/middleware/authMiddleware.js
@@ -4,15 +4,16 @@ const userModel = require('../../models/userModel');
 const authenticate = (req, res, next) => {
     const token = req.headers.authorization?.split(' ')[1];
     if (!token) {
-        return res.status(401).send('Access denied. No token provided.');
+        return res.status(401).json({ message: 'Access denied. No token provided.' });
     }
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET || 'mysecretkey_12345');
-        req.user = decoded; // Assuming your JWT has user info, particularly `id`
+        req.user = decoded;  // Assuming your JWT contains user info
         next();
     } catch (ex) {
-        res.status(400).send('Invalid token.');
+        res.status(400).json({ message: 'Invalid token.' });
     }
 };
+
 module.exports = authenticate;
