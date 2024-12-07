7c797b94fee8691ff61d2679d3a94d43a336e587
Vinzski <153497509+Vinzski@users.noreply.github.com>
2024-12-08 02:28:09 +0800

    Update faqModel.js

--- src/node/models/faqModel.js
+++ src/node/models/faqModel.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
+// Define the FAQ schema
 const faqSchema = new mongoose.Schema({
     question: { type: String, required: true },
     answer: { type: String, required: true },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-});
+}, { timestamps: true });
 
+// Create and export the FAQ model
 const FAQ = mongoose.model('FAQ', faqSchema);
+module.exports = FAQ;
