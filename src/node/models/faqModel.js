b1a1b11d9a05a7656e3aa5a1b0e0e8273ee21e20
Vinzski <153497509+Vinzski@users.noreply.github.com>
2024-12-07 23:24:22 +0800

    Update faqModel.js

--- src/node/models/faqModel.js
+++ src/node/models/faqModel.js
@@ -4,9 +4,6 @@ const faqSchema = new mongoose.Schema({
     question: { type: String, required: true },
     answer: { type: String, required: true },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    chatbotId: { type: mongoose.Schema.Types.ObjectId, ref: 'Chatbot', required: true }, // New Field
-}, { timestamps: true }); // Optional: Adds createdAt and updatedAt fields
+});
 
 const FAQ = mongoose.model('FAQ', faqSchema);
-
-module.exports = FAQ;
