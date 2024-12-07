c7a1a14f366e79ad28f4b48a538c925f8ea428d3
Vinzski <153497509+Vinzski@users.noreply.github.com>
2024-12-07 22:27:43 +0800

    Update chatRoutes.js

--- src/node/api/chatRoutes.js
+++ src/node/api/chatRoutes.js
@@ -66,7 +66,7 @@ router.post('/', authenticateToken, async (req, res) => {
         } else {
             console.log("No suitable FAQ match found. Querying Rasa.");
             // Fallback to Rasa
-            const rasaURL = 'https://silver-walls-repeat.loca.lt/webhooks/rest/webhook'; // Update to your Rasa URL
+            const rasaURL = 'https://small-words-pay.loca.lt/webhooks/rest/webhook'; // Update to your Rasa URL
             const rasaResponse = await axios.post(rasaURL, {
                 message: question,
                 sender: 'chatbot-widget'
