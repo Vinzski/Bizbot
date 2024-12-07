39cfcd9796a195891b55b5ccce007c5cf1117784
Vinzski <153497509+Vinzski@users.noreply.github.com>
2024-12-07 19:03:47 +0800

    Update authcontroller.js

--- src/node/signup/controllers/authcontroller.js
+++ src/node/signup/controllers/authcontroller.js
@@ -2,6 +2,10 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../../models/userModel'); // Ensure the path to userModel is correct
 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../../models/userModel'); // Ensure the path to userModel is correct
+
 exports.signup = async (req, res) => {
     console.log('Signup data received:', req.body);
     const { username, email, password } = req.body;
