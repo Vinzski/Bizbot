1be09950ba0b94b168fd1bdac1dd8789c6ac5344
Vinzski <153497509+Vinzski@users.noreply.github.com>
2024-12-09 05:51:08 +0800

    Update domainModel.js

--- src/node/models/domainModel.js
+++ src/node/models/domainModel.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const domainSchema = new mongoose.Schema({
     domain: { type: String, required: true, unique: true },
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }]
-}, { timestamps: true });
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, 
+    { timestamps: true });
 });
 
 const Domain = mongoose.model('Domain', domainSchema);
