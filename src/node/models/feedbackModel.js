69b580d02b670a0aad7876afc569d8f457f778a4
Vinzski <153497509+Vinzski@users.noreply.github.com>
2024-12-09 06:52:53 +0800

    Update feedbackModel.js

--- src/node/models/feedbackModel.js
+++ src/node/models/feedbackModel.js
@@ -4,9 +4,10 @@ const feedbackSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
   chatbotId: { type: String, required: true, ref: 'Chatbot' },
   rating: { type: String, required: true },
-  feedbackText: { type: String, required: true, },
+  feedback: { type: String, required: true, },
   createdAt: { type: Date, default: Date.now },
 });
 
 const Feedback = mongoose.model('Feedback', feedbackSchema);
 module.exports = Feedback;
+
