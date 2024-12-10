dc6c85e677518a0177b0fb765cb3a6f5cca762f5
Vinzski <153497509+Vinzski@users.noreply.github.com>
2024-12-10 13:47:47 +0800

    Update customizationRoutes.js

--- src/node/api/customizationRoutes.js
+++ src/node/api/customizationRoutes.js
@@ -9,7 +9,7 @@ const bcrypt = require('bcrypt');
 
 // File upload configuration
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, '../../../uploads/'),
+    destination: (req, file, cb) => cb(null, '../shared/'),
     filename: (req, file, cb) => cb(null, `${Date.now()}_${file.originalname}`)
 });
 const upload = multer({ storage });
