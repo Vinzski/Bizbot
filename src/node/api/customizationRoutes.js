1902f3e3cfc2f005cd0f1a735e7708a615886c93
Vinx <153497509+Vinzski@users.noreply.github.com>
2024-12-25 20:22:09 +0800

    Update customizationRoutes.js

--- src/node/api/customizationRoutes.js
+++ src/node/api/customizationRoutes.js
@@ -14,6 +14,7 @@ const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION, // e.g., 'us-east-1'
+    bucket_name: process.env.AWS_S3_BUCKET_NAME
 });
 
 // Multer S3 storage configuration
