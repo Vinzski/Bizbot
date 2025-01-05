1925989abecb00ab3f6ec581324a3f5fde4d9c38
Vinx <153497509+Vinzski@users.noreply.github.com>
2025-01-05 13:45:52 +0800

    Update chatbotModel.js

--- src/node/models/chatbotModel.js
+++ src/node/models/chatbotModel.js
@@ -4,8 +4,9 @@ const chatbotSchema = new mongoose.Schema({
     name: { type: String, required: true },
     type: { type: String, required: true },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    faqs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'FAQ' }] }, 
-    { timestamps: true });  // Enables automatic handling of createdAt and updatedAt
+    faqs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'FAQ' }],
+    pdfs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'PDF' }]
+}, { timestamps: true });
 
 const Chatbot = mongoose.model('Chatbot', chatbotSchema);
 
